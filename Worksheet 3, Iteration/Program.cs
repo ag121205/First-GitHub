8154d77a316d2244fa6dc3c1792a7342c67ac70d
Alan George <alangeorge121205@gmail.com>
2023-01-31 19:18:40 +0000

    Fixed the bug

---
+++ Worksheet 3, Iteration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Worksheet_3__Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+
+            string[] name = { "Hello", "Goodbye", "Goodmorning", "Goodnight" };
+
+            for(counter = 0; counter <=3; counter++)
+            {
+                Console.WriteLine(name[counter]);
+            }
+            
+        }
+    }
+}
